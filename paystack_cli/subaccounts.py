8f37462667fdce971bce96d390feaa89a6c4750d
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-07-21 16:06:16 +0100

    Implement subaccounts command

---
+++ paystack_cli/subaccounts.py
@@ -0,0 +1,84 @@
+from typing import Optional
+
+from pypaystack2 import Schedule
+from typer import Typer
+
+from paystack_cli.utils import get_paystack_wrapper, parse_cli_string
+
+subaccount_app = Typer()
+
+
+@subaccount_app.command()
+def create(
+    business_name: str,
+    settlement_bank: str,
+    account_number: str,
+    percentage_charge: float,
+    description: Optional[str] = None,
+    primary_contact_email: Optional[str] = None,
+    primary_contact_name: Optional[str] = None,
+    primary_contact_phone: Optional[str] = None,
+    metadata: Optional[str] = None,
+):
+    if metadata:
+        metadata = parse_cli_string(
+            raw_string=metadata, arg_or_option_name="metadata", expected_type=dict
+        )
+    return get_paystack_wrapper().subaccounts.create(
+        business_name=business_name,
+        settlement_bank=settlement_bank,
+        account_number=account_number,
+        percentage_charge=percentage_charge,
+        description=description,
+        primary_contact_email=primary_contact_email,
+        primary_contact_name=primary_contact_name,
+        primary_contact_phone=primary_contact_phone,
+        metadata=metadata,
+    )
+
+
+@subaccount_app.command()
+def update(
+    id_or_code: str,
+    business_name: str,
+    settlement_bank: str,
+    account_number: Optional[str] = None,
+    active: Optional[bool] = None,
+    percentage_charge: Optional[float] = None,
+    description: Optional[str] = None,
+    primary_contact_email: Optional[str] = None,
+    primary_contact_name: Optional[str] = None,
+    primary_contact_phone: Optional[str] = None,
+    settlement_schedule: Optional[Schedule] = None,
+    metadata: Optional[str] = None,
+):
+    if metadata:
+        metadata = parse_cli_string(
+            raw_string=metadata, arg_or_option_name="metadata", expected_type=dict
+        )
+    return get_paystack_wrapper().subaccounts.update(
+        id_or_code=id_or_code,
+        business_name=business_name,
+        settlement_bank=settlement_bank,
+        account_number=account_number,
+        active=active,
+        percentage_charge=percentage_charge,
+        description=description,
+        primary_contact_email=primary_contact_email,
+        primary_contact_name=primary_contact_name,
+        primary_contact_phone=primary_contact_phone,
+        settlement_schedule=settlement_schedule,
+        metadata=metadata,
+    )
+
+
+@subaccount_app.command()
+def get_subaccount(id_or_code: str):
+    return get_paystack_wrapper().subaccounts.get_subaccount(id_or_code=id_or_code)
+
+
+@subaccount_app.command()
+def get_subacconts(start_date: str, end_date: str, page: int = 1, pagination: int = 50):
+    return get_paystack_wrapper().subaccounts.get_subaccounts(
+        start_date=start_date, end_date=end_date, page=page, pagination=pagination
+    )
